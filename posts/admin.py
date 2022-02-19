ae98bf758cf622c2182577922800310dc4ec2b48
rom7tro <rom7tro@gmail.com>
2022-02-19 15:34:08 +0300

    закончены все тесты

--- posts/admin.py
+++ posts/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Group, Post
 
 
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("pk", "text", "pub_date", "author")
+    list_display = ("pk", "text", "pub_date", "author", "group")
     search_fields = ("text",)
     list_filter = ("pub_date",)
     empty_value_display = "-пусто-"
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("title",)
+    list_filter = ("title",)
+    prepopulated_fields = {"slug": ("title",)}
+    empty_value_display = "-пусто-"
+    
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+
--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,31 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
+    class Meta:
+        ordering = ["-pub_date"]
+
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="posts",
+        )
+    group = models.ForeignKey(
+        Group, 
+        on_delete=models.SET_NULL,
+        related_name="group_posts",
+        blank=True, 
+        null=True,        
+        )
+    def __str__(self):
+        return self.text
